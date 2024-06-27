3006b8b9acb18d2027b457885bb9395254c58548
Akash Kumar <meakash7902@gmail.com>
2024-06-28 05:16:34 +0530

    changes
    Signed-off-by: Akash Kumar <meakash7902@gmail.com>

--- benchmarks/run_tardis.py
+++ benchmarks/run_tardis.py
@@ -4,6 +4,7 @@ Basic TARDIS Benchmark.
 
 from benchmarks.benchmark_base import BenchmarkBase
 from tardis import run_tardis
+from tardis.io.atom_data.util import download_atom_data
 from tardis.io.configuration.config_reader import Configuration
 
 
@@ -14,12 +15,11 @@ class BenchmarkRunTardis(BenchmarkBase):
 
     def __init__(self):
         super().__init__()
-        self.config = None
-
-    def setup(self):
         filename = "data/tardis_configv1_benchmark.yml"
-        path = self.get_relative_path(filename)
-        self.config = Configuration.from_yaml(path)
+        self.path = self.get_relative_path(filename)
+        download_atom_data('kurucz_cd23_chianti_H_He')
 
     def time_run_tardis(self):
-        run_tardis(self.config, log_level="ERROR", show_progress_bars=False)
+        run_tardis(self.path, log_level="ERROR", show_progress_bars=False)
+
+
