65ddf8a1838fd42e6d52cff28a44a9787605eb65
Akash Kumar <meakash7902@gmail.com>
2024-06-28 05:54:33 +0530

    changes

--- benchmarks/run_tardis.py
+++ benchmarks/run_tardis.py
@@ -2,20 +2,103 @@
 Basic TARDIS Benchmark.
 """
 
+from asv_runner.benchmarks.mark import parameterize
+
+import tardis.opacities.opacities as calculate_opacity
 from benchmarks.benchmark_base import BenchmarkBase
-from tardis import run_tardis
 
-class BenchmarkRunTardis(BenchmarkBase):
+
+class BenchmarkMontecarloMontecarloNumbaOpacities(BenchmarkBase):
     """
-    Class to benchmark the `run tardis` function.
+    Class to benchmark the numba opacities function.
     """
 
-    def __init__(self):
-        super().__init__()
-        filename = "data/tardis_configv1_benchmark.yml"
-        self.path = self.get_relative_path(filename)
+    @parameterize(
+        {
+            "Electron number density": [
+                1.0e11,
+                1e15,
+                1e5,
+            ],
+            "Energy": [
+                511.0,
+                255.5,
+                511.0e7,
+            ],
+        }
+    )
+    def time_compton_opacity_calculation(self, electron_number_density, energy):
+        calculate_opacity.compton_opacity_calculation(
+            energy, electron_number_density
+        )
 
-    def time_run_tardis(self):
-        run_tardis(self.path, log_level="ERROR", show_progress_bars=False)
+    @parameterize(
+        {
+            "Ejecta density": [
+                1.0,
+                1e-2,
+                1e-2,
+                1e5,
+            ],
+            "Energy": [
+                511.0,
+                255.5,
+                255.5,
+                511.0e7,
+            ],
+            "Iron group fraction": [
+                0.0,
+                0.5,
+                0.25,
+                1.0,
+            ],
+        }
+    )
+    def time_photoabsorption_opacity_calculation(
+        self, ejecta_density, energy, iron_group_fraction
+    ):
+        calculate_opacity.photoabsorption_opacity_calculation(
+            energy, ejecta_density, iron_group_fraction
+        )
 
+    @parameterize(
+        {
+            "Ejecta density": [
+                1.0,
+                1e-2,
+                1e-2,
+                1e5,
+            ],
+            "Energy": [
+                511.0,
+                1500,
+                1200,
+                511.0e7,
+            ],
+            "Iron group fraction": [
+                0.0,
+                0.5,
+                0.25,
+                1.0,
+            ],
+        }
+    )
+    def time_pair_creation_opacity_calculation(
+        self, ejecta_density, energy, iron_group_fraction
+    ):
+        calculate_opacity.pair_creation_opacity_calculation(
+            energy, ejecta_density, iron_group_fraction
+        )
 
+    @parameterize(
+        {
+            "Energy": [
+                511.0, 
+                255.5, 
+                0.0, 
+                511.0e7,
+            ]
+        }
+    )
+    def time_kappa_calculation(self, energy):
+        calculate_opacity.kappa_calculation(energy)
